c6000713bc1eaf1acc26d7fbc624d410bf1a7094
anamerasul <anamerasul@gmail.com>
2022-05-01 03:04:17 +0600

    all update

--- notes-taker-react/src/App.js
+++ notes-taker-react/src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/header/Header";
 import InputForm from "./components/inputForm/InputForm";
@@ -10,19 +10,40 @@ import { useEffect, useState } from "react";
 function App() {
   const [notes, setNotes] = useState([]);
 
+  const [isReload,setIsReload]=useState(false)
+
   useEffect(() => {
 
-   fetch("notes.json")
+   fetch(`http://localhost:3005/notes`)
    .then(res=>res.json())
    .then(data=>setNotes(data))
+  //  .then(data=>console.log(data))
 
     
-  }, []);
+  }, [isReload]);
   /*
 1. here there will be a function named handleSearch
 to handle search by query, and it will be passed as props to header
 
   */
+
+const handleSearch = (event)=>{
+  event.preventDefault()
+  // console.log('working')
+
+  const queryText=event.target.searchText.value
+
+  // console.log('hello',queryText)
+
+  if (queryText){
+    fetch(`http://localhost:3005/notes?userName=${queryText}`)
+    .then(res=>res.json())
+    .then(data=>setNotes(data))
+   
+  }
+
+
+}
   
 
 
@@ -62,15 +83,50 @@ to delete a note, and it will be passed as props to NoteCard that will be trigge
 to post data to backend, and it will be passed as props to InputFrom.
  */
 
+
+const handlePost =(event)=>{
+
+  event.preventDefault()
+const userName=event.target.userName.value
+const testdata=event.target.testData.value
+  console.log(userName,testdata)
+
+  // if(userName&&testdata){
+
+    fetch(`http://localhost:3005/notes`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+    
+      body: JSON.stringify({userName,testdata}),
+     
+    })
+      .then((response) => response.json())
+      .then((data) =>{
+
+        setIsReload(!isReload)
+
+      } );
+
+  // }
+
+  event.target.reset();
+
+
+
+}
   
 
   return (
     <div className="App">
-      <Header  />
-      <InputForm />
+      <Header handleSearch={handleSearch}  />
+      <InputForm handlePost={handlePost} />
       <div className="row row-cols-1 row-cols-md-3 g-4 m-2">
         {notes.map((note) => (
           <NoteCard
+
+          key={note._id}
             note={note}
           />
         ))}
